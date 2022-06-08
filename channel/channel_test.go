16961d7c1d48f326f15d0c3f38d39a0f45e56f2a
ByLele <2516066049@qq.com>
2022-06-08 22:20:46 +0800

    Default Changelist

--- channel/channel_test.go
+++ channel/channel_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 	"time"
 )
+
 /*func TestChannel(t *testing.T) {
 	message := make(chan string)
 	go func() {
@@ -14,24 +15,24 @@ import (
 	fmt.Println(msg)
 }*/
 func TestChannelSend(t *testing.T) {
-	ch := make(chan interface{})
+	ch := make(chan string)
 
 	go func() {
-		ch<-"goroutine send"
+		ch <- "goroutine send"
 	}()
 	ch1 := <-ch
 	fmt.Println(ch1)
 }
-func TestRecyclingChannel(t * testing.T) {
+func TestRecyclingChannel(t *testing.T) {
 	ch := make(chan interface{})
-	go func(){
-		for i:=0;i<=3;i++ {
+	go func() {
+		for i := 0; i <= 3; i++ {
 			ch <- i
 			time.Sleep(time.Second)
 		}
 		close(ch)
 	}()
-	for msg:= range ch {
+	for msg := range ch {
 		fmt.Println(msg)
 		if msg == 3 {
 			break
@@ -39,7 +40,7 @@ func TestRecyclingChannel(t * testing.T) {
 	}
 }
 func TestChannelBuffer(t *testing.T) {
-	message := make(chan string,2)//两个缓冲
+	message := make(chan string, 2) //两个缓冲
 	message <- "test channl buffer 1"
 	message <- "test channel buffer 2"
 	fmt.Println(<-message)
@@ -55,17 +56,18 @@ func worker(done chan bool) {
 	//结束阻塞
 	done <- true
 }
-func TestChannelSync(t* testing.T) {
+func TestChannelSync(t *testing.T) {
 	done := make(chan bool)
 	go worker(done)
 
 	<-done
 }
+
 //TestProAndCus
 func Printer(c chan int) {
 	for {
 		msg := <-c
-		if(msg == 0) {
+		if msg == 0 {
 			break
 		}
 
@@ -73,7 +75,7 @@ func Printer(c chan int) {
 	}
 	c <- -1
 }
-func TestProAndCus(t* testing.T) {
+func TestProAndCus(t *testing.T) {
 
 	c := make(chan int)
 	go Printer(c)
@@ -88,11 +90,32 @@ func TestProAndCus(t* testing.T) {
 }
 
 //在goroutine执行前程序结束运行
-func TestGorou(t*testing.T) {
+func TestGorou(t *testing.T) {
 	go println("goroutine message")
 	println("main function message")
 }
+
 //TODO channel 中值channel  make(chan <- chan bool)
 
+// muliti channel receive
+func getMessage(order string, delay time.Duration) chan string {
+	c := make(chan string)
+	go func() {
+		for i := 1; i < 3; i++ {
+			c <- fmt.Sprintf("%s %d", order, i)
+			time.Sleep(delay)
+		}
+	}()
+	return c
+}
+func TestMultiChannel(t *testing.T) {
+	c1 := getMessage("first", 30)
+	c2 := getMessage("second", 150)
+	c3 := getMessage("third", 100)
 
-
+	for i := 1; i < 3; i++ {
+		fmt.Println(<-c1)
+		fmt.Println(<-c2)
+		fmt.Println(<-c3)
+	}
+}
